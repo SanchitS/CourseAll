06cdd46e5df599f54e88681c152f6b01e14730ec
sanchits <sanchitsharma1209@gmail.com>
2015-07-17 20:21:22 +0530

    typeahead.js , search box

---
+++ js/style.js
@@ -0,0 +1,30 @@
+var bestPictures = new Bloodhound({
+    datumTokenizer: Bloodhound.tokenizers.obj.whitespace('value'),
+    queryTokenizer: Bloodhound.tokenizers.whitespace,
+    prefetch: '../data/films/post_1960.json',
+    remote: {
+        url: '../data/films/queries/%QUERY.json',
+        wildcard: '%QUERY'
+    }
+});
+
+$('#remote .typeahead').typeahead(null, {
+    name: 'best-pictures',
+    display: 'value',
+    source: bestPictures
+});
+
+
+$('#custom-templates .typeahead').typeahead(null, {
+    name: 'best-pictures',
+    display: 'value',
+    source: bestPictures,
+    templates: {
+        empty: [
+      '<div class="empty-message">',
+        'unable to find any Best Picture winners that match the current query',
+      '</div>'
+    ].join('\n'),
+        suggestion: Handlebars.compile('<div><strong>{{value}}</strong> – {{year}}</div>')
+    }
+});
